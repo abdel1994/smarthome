fdb573ef91ee8bf5bebce819fed3501eb03ef5c5
Abdelmajid el Hajoui <abdel1994@live.nl>
2025-03-24 16:34:09 +0100

    Initial commit

--- smarthome/Program.cs
+++ smarthome/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
